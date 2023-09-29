6a990004c9198c044cb3a5dd2f8bab69c2a1e6da
Leonardo basilio <ew.basilio@gmail.com>
2023-09-29 15:15:09 -0300

    implementcao das leituras de prazo

--- Controlers/mailcontroler.py
+++ Controlers/mailcontroler.py
@@ -3,7 +3,7 @@ from datetime import date
 from JJBot import bot
 from time import sleep
 import re
-
+import sys
 
 
 __mail = "email"
@@ -11,44 +11,98 @@ __pswd = "senha"
 
 mails = ["lista de emails"]
 materias = ["Teste", "Processos Ágeis de Desenvolvimento", "Ambientação Digital", "Avaliação Integrada de Competências em Coding I", "Plano de Acompanhamento de Carreira em Coding I", "Estrutura de Dados", "Modelagem de Dados", "Fundamentos de Devops", "Linguagem de Banco de Dados", "Programação Orientada a Objetos"]
+
+padrao_url = 'https?://\S+|www\.\S+'
+padrao_date = '\d{1,2}/\d{1,2}/\d{4}'
+
 while True:
 
     try:
         with MailBox("imap.gmail.com").login(__mail, __pswd) as mailbox:
-            print("mail entre")
-            for msg in mailbox.fetch(AND(OR(text=mails), AND(date=date.today(), seen=False))):
+            print("inbox")
+            inbox = mailbox.fetch(AND(OR(text=mails), AND(seen=False)))
+            print("tamaho do inbox: ", sys.getsizeof(inbox))
+            for msg in inbox:
                 for mat in materias:
+                    print(mat)
                     subject = re.search(mat, msg.subject)
-                    link = re.search("http", msg.text)
-                    prova = (re.search("Prova", msg.subject) or re.search("Prova", msg.text))
+                    print(subject)
+                    prazo = (re.search("prazo", msg.text) or re.search("Prazo", msg.text))
+                    print(prazo)
+
+                    link = re.findall(padrao_url, msg.text)
+                    print(link)
+                    prazos_date = re.findall(padrao_date, msg.text)
+                    print(prazos_date)    
+
+                    prova = (re.search("Prova", msg.subject) or re.search("Prova", msg.text) or re.search("prova", msg.text))
+                    print(prova)
                     af = (re.search("AF", msg.subject,) or re.search("AF", msg.text))
+                    print(af)
+                    
+                    
                     if subject:
                         if prova or af:
-                            print("###### Prova De ", mat, "######")
-                            print(msg.text)
+                            print("Prova De ", mat)
                             
-                            botmsg = "###### Prova De {} ###### \n {}".format(mat, msg.text)
+                            botmsg = "Prova De {} \n {}".format(mat, msg.text)
 
-                            if link:
-                                print("contem um link")
-                                
-                                botmsg = "###### Prova De {} ###### \n {} \n Contem um link que pode nao estar anexado na notificação".format(mat, msg.text)
+                            if prazo:
+
+                                if link:
+                                    print("contem um link")
+                                    botmsg = "Prova De {} \n datas: {} \n link: {}".format(mat, prazos_date, link)
+
+                                botmsg = "Prova De {} \n datas: {}".format(mat, prazos_date)
+
+                            elif link:
+
+                                botmsg = "Prova De {} \n link: {}".format(mat, link)
+
                             
                             bot(botmsg)
+                            sleep(120)
+
                         elif not (prova or af):
-                            print("chama api para materia comum passnasdo a materia", mat)
+                            print("materia", mat)
                             
-                            botmsg = "###### Nova atividade na materia {} ######".format(mat)
+                            botmsg = "Nova atividade de {} ".format(mat)
 
-                            if link:
-                                print("contem link")
-                            
-                                botmsg = "###### Nova atividade na materia {} ###### \n {} \n Contem um link que pode nao estar anexado na notificação".format(mat, msg.text)
+                            if prazo:
+
+                                if link:
+                                    print("contem um link")
+                                    botmsg = "Nova atividade de {} \n datas: {} \n link: {}".format(mat, prazos_date, link)
+
+                                botmsg = "Nova atividade de {} \n datas: {}".format(mat, prazos_date)
+
+                            elif link:
+
+                                botmsg = "Nova atividade de {} \n link: {}".format(mat, link)
 
                             bot(botmsg)
+                            sleep(120)
 
-        sleep(1200)
+                        else:
+                            print("materia", mat)
+                            
+                            botmsg = "Nova atividade de {} ".format(mat)
 
+                            if prazo:
+
+                                if link:
+                                    print("contem um link")
+                                    botmsg = "Nova atividade de {} \n datas: {} \n link: {}".format(mat, prazos_date, link)
+
+                                botmsg = "Nova atividade de {} \n datas: {}".format(mat, prazos_date)
+
+                            elif link:
+
+                                botmsg = "Nova atividade de {} \n link: {}".format(mat, link)
+
+                            bot(botmsg)
+                            sleep(120)
+                    
     except Exception as e:
         print(e)
         break
